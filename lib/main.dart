351885eb5d27040c5f53e4bd43a610f4cc1bec38
dabuzida <dabuzida@gmail.com>
2022-12-13 17:14:06 +0900

    start

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+import 'my_divider.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Checkbox',
+      home: MyContainer(),
+    );
+  }
+}
+
+class MyContainer extends StatelessWidget {
+  const MyContainer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Checkbox'),
+        elevation: 0,
+        foregroundColor: Colors.white,
+        backgroundColor: Colors.teal,
+        centerTitle: true,
+      ),
+      body: MyDivider(),
+    );
+  }
+}
